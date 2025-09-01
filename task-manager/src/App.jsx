acfac31d2459a2ebb69268a43e3df626ed3a606c
brian-w119 <brianw119@outlook.com>
2025-09-01 19:04:12 +0100

    Add job details box created

--- task-manager/src/App.jsx
+++ task-manager/src/App.jsx
@@ -1,8 +1,36 @@
 import "./App.css";
 import SignInForm from "./sign-in";
+import logo from "./assets/logo.jpg";
+
+export const Form = ({ id, label, type }) => {
+  return (
+    <form className="form-field">
+      <label className="field" htmlFor={id}>
+        {label}
+      </label>
+      <input className="field" type={type} id={id} name={id} />
+    </form>
+  );
+};
+
+const Outline = () => {
+  return (
+    <>
+      <img src={logo}></img>
+      <div className="outline">
+        <Form id="name" label="Client's Name" type="text" />
+        <textarea
+          rows={10}
+          placeholder="Enter job details"
+          className="text-area"
+        />
+      </div>
+    </>
+  );
+};
 
 const App = () => {
-  return <SignInForm />;
+  return <Outline className="form" />;
 };
 
 export default App;
---
+++ task-manager/src/log-in-page.jsx
@@ -0,0 +1,9 @@
+import logo from "./assets/logo.jpg";
+import { Form } from "./App";
+
+const Log-in-box = () => {
+    return (
+        
+    )
+}
+
